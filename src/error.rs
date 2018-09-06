b234011fc25dd1366dc06eed8a3c9f13d8ae6298
Kyle Clemens <git@kyleclemens.com>
2018-09-06 11:31:11 -0400

    feat: log internal errors

--- src/error.rs
+++ src/error.rs
@@ -40,7 +40,10 @@ impl<T> From<Result<T, Error>> for RouteResult<T> {
         "invalid page (1 through {} available)",
         page,
       )),
-      Err(_) => RouteResult::error("an internal error occurred. did the lodestone change?"),
+      Err(e) => {
+        eprintln!("error: {:#?}", e);
+        RouteResult::error("an internal error occurred. did the lodestone change?")
+      },
     }
   }
 }
